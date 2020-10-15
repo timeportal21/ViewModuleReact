b622ebf8d66e149b91f52d61c0b58382357fee11
timeportal21 <timeportal21@gmail.com>
2020-10-16 00:34:37 +0545

    [add] UI of login and register

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,9 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+    const [loginData, setLoginData] = useState({
+        email: "",
+        password: "",
+    });
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    const onChange = (e) => {
+        setLoginData({ ...loginData, [e.target.name]: e.target.value });
+    };
+    const { email, password } = loginData;
     return (
-        <div>
-            <p>this is login page</p>
+        <div className="register__form">
+            <div className="auth__box">
+                <div className="info__header">
+                    <h1 className="large text-center">Login In</h1>
+                    <p className="lead text-center">Access Your Account</p>
+                </div>
+                <div className="form__box">
+                    <form className="form" onSubmit={handleSubmit}>
+                        <div className="form-group">
+                            <input
+                                type="email"
+                                className="form-control"
+                                placeholder="Email Address"
+                                name="email"
+                                value={email}
+                                onChange={onChange}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <input
+                                type="password"
+                                className="form-control"
+                                placeholder="Password"
+                                name="password"
+                                value={password}
+                                onChange={onChange}
+                            />
+                        </div>
+                        <button
+                            type="submit"
+                            className="btn btn-primary btn-block"
+                        >
+                            Login
+                        </button>
+                    </form>
+                </div>
+                <p className="my-1">
+                    Dont't have an account?
+                    <Link to="/register">Register</Link>
+                </p>
+            </div>
         </div>
     );
 };
--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,9 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Register.css";
+import { Link } from "react-router-dom";
 
 const Register = () => {
+    const [registerData, setRegisterData] = useState({
+        name: "",
+        email: "",
+        password: "",
+        password2: "",
+    });
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    const onChange = (e) => {
+        setRegisterData({ ...registerData, [e.target.name]: e.target.value });
+    };
+    const { name, email, password, password2 } = registerData;
     return (
-        <div>
-            <p>this is register page</p>
+        <div className="register__form">
+            <div className="auth__box">
+                <div className="info__header">
+                    <h1 className="large text-center">Register</h1>
+                    <p className="lead text-center">Create Your Account</p>
+                </div>
+                <div className="form__box">
+                    <form className="form" onSubmit={handleSubmit}>
+                        <div className="form-group">
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Name"
+                                name="name"
+                                value={name}
+                                onChange={onChange}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <input
+                                type="email"
+                                className="form-control"
+                                placeholder="Email Address"
+                                name="email"
+                                value={email}
+                                onChange={onChange}
+                            />
+                            <small className="form-text text-muted">
+                                This site uses Gravatar so if you want a profile
+                                image, use a Gravatar email
+                            </small>
+                        </div>
+                        <div className="form-group">
+                            <input
+                                type="password"
+                                className="form-control"
+                                placeholder="Password"
+                                name="password"
+                                value={password}
+                                onChange={onChange}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <input
+                                type="password"
+                                className="form-control"
+                                placeholder="Confirm Password"
+                                name="password2"
+                                value={password2}
+                                onChange={onChange}
+                            />
+                        </div>
+                        <button
+                            type="submit"
+                            className="btn btn-primary btn-block"
+                        >
+                            Register
+                        </button>
+                    </form>
+                </div>
+                <p className="my-1">
+                    Already have an account? <Link to="/login">Sign In</Link>
+                </p>
+            </div>
         </div>
     );
 };
